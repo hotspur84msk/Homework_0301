db72df15953f0cc144b1b9b582ffa472fbe5537c
Sergei.Petrushin <sjabrail@gmail.com>
2023-04-22 14:32:20 +0530

    prorgam is done

--- hw_0301_task8/Program.cs
+++ hw_0301_task8/Program.cs
@@ -3,3 +3,17 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num;
+
+Console.Write($"Чётные числа от 1 до {num} -> ");
+
+for (int i = 0; i < num; i++)
+{
+    if(result % 2 == 0)
+    {
+        Console.Write($"{result}, ");
+    }
+    result--;
+}
